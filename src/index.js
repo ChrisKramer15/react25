81971edf41f1335607b3efcfd2e7471d5ca04857
Christopher <syntella.development@gmail.com>
2025-02-14 18:10:59 -0600

    practiced alternate ways to structure react functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// const Greeting = () => {
+//     return <h2>My First Component</h2>
+// }
+
+// function Greeting() {
+//     return React.createElement('h2', {}, 'hello world');
+// }
+
+// function Greeting() {
+//     return(
+//         <div>
+//             <h2>My First Component</h2>;
+//         </div>
+//     )
+// }
+
 const Greeting = () => {
-    return <h2>My First Component</h2>
+  return (
+    React.createElement('div', {}, 
+        React.createElement('h2', {}, 'hello world'),
+    )
+  )
 }
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
