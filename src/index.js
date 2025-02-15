29f3e4c8c0519841ba18374ece0c75ea5d265be9
Christopher <syntella.development@gmail.com>
2025-02-15 16:08:04 -0600

    basic level css implemented, grid system added to book component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import './index.css';
+
 const BookList = () => {
-  return <section>
+  return <section className='booklist'>
     <Book/>
     <Book/>
     <Book/>
@@ -11,14 +13,14 @@ const BookList = () => {
 }
 
 const Book = () => {
-  return <article>
+  return <article className='book'>
     <Image/>
     <Title/>
     <Author/>
   </article>
 }
 
-const Image = () => <img src='https://m.media-amazon.com/images/I/91i3L7p7tIL._SY342_.jpg'/>
+const Image = () => <img src='https://m.media-amazon.com/images/I/91i3L7p7tIL._SY342_.jpg' alt='Santa Paws'/>
 const Title = () => <h2>Santa Paws</h2>
 const Author = () => {
   return <h4>Nicholas Edwards</h4>
